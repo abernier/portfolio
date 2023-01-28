ad0bf9e9cc6b53fbfcdafe9caa476e45c0842287
Antoine BERNIER <antoine.bernier@gmail.com>
2023-01-29 00:33:53 +0100

    upgrade drei + tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import * as THREE from "three";
 import styled from "@emotion/styled";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
 import Layout from "./Layout";
 import {
   CameraControls,
+  CatmullRomLine,
   Sparkles,
   useKeyboardControls,
 } from "@react-three/drei";
+import { Line2 } from "three-stdlib";
 
-import CameraControlsImpl from "camera-controls";
+// import CameraControlsImpl from "camera-controls";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -26,18 +28,18 @@ gsap.registerPlugin(GSDevTools);
 // globalThis.gsap = gsap;
 
 function App() {
-  const [tl] = useState(() => gsap.timeline({ id: "main", paused: true }));
-  globalThis.tl = tl;
+  // const [tl] = useState(() => gsap.timeline({ id: "main", paused: true }));
+  // globalThis.tl = tl;
 
-  useEffect(() => {
-    console.log("GSDevTools");
-    const gsDevTools = GSDevTools.create({ animation: tl });
+  // useEffect(() => {
+  //   console.log("GSDevTools");
+  //   const gsDevTools = GSDevTools.create({ animation: tl });
 
-    return () => {
-      console.log("kill");
-      gsDevTools.kill();
-    };
-  }, [tl]);
+  //   return () => {
+  //     console.log("kill");
+  //     gsDevTools.kill();
+  //   };
+  // }, [tl]);
 
   return (
     <Styled>
@@ -57,7 +59,9 @@ export default App;
 
 function Scene() {
   const cameraControlsRef = useRef<CameraControls | null>(null);
-  // globalThis.cameraControlsRef = cameraControlsRef;
+  // const controls = useThree(
+  //   (state) => state.controls as unknown as CameraControls
+  // );
 
   useFrame(({ clock }) => gsap.updateRoot(clock.getElapsedTime()));
 
@@ -76,40 +80,9 @@ function Scene() {
   });
   // globalThis.video = video;
 
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      const tl = gsap.timeline({ paused: true });
-
-      const twVideo = gsap.fromTo(
-        video,
-        { currentTime: 0 },
-        { currentTime: 93, duration: 93, ease: "linear" }
-      );
-
-      tl.add(twVideo);
-
-      tl.play();
-    });
-
-    return () => ctx.revert();
-  }, [video]);
-
-  // const scrollTrigger = useMemo(
-  //   () =>
-  //     ScrollTrigger.create({
-  //       trigger: document.body,
-  //       start: "top top",
-  //       end: "bottom bottom",
-  //       markers: true,
-  //       // snap: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], // snap to 10% increments
-  //       scrub: 1,
-  //       onUpdate(st) {
-  //         // console.log("scrollTrigger", st.progress);
-  //         tw.progress(st.progress);
-  //       },
-  //     }),
-  //   []
-  // );
+  //
+  // video texture
+  //
 
   useEffect(() => {
     console.log("useEffect video", video);
@@ -133,6 +106,81 @@ function Scene() {
 
     return texture;
   }, [video]);
+
+  //
+  // gsap
+  //
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({ paused: true });
+
+      //
+      // twVideo
+      //
+
+      const twVideo = gsap.fromTo(
+        video,
+        { currentTime: 0 },
+        { currentTime: 93, duration: 93, ease: "linear", paused: true }
+      );
+
+      ScrollTrigger.create({
+        trigger: document.body,
+        start: "top top",
+        end: "bottom bottom",
+        markers: true,
+        snap: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], // snap to 10% increments
+        scrub: 1,
+        onUpdate(st) {
+          // console.log("scrollTrigger", st.progress);
+          twVideo.progress(st.progress);
+        },
+      });
+
+      tl.add(twVideo);
+
+      //
+      // twCamera
+      //
+
+      const twCamera = gsap.timeline({ paused: true });
+
+      //
+      //
+      //
+
+      tl.play();
+    });
+
+    return () => ctx.revert();
+  }, [video]);
+
+  const cameraControlsCallbackRef = useCallback((instance: CameraControls) => {
+    cameraControlsRef.current = instance;
+    if (!instance) return;
+
+    instance.setTarget(0, 3, 0);
+    // instance.mouseButtons.wheel = CameraControlsImpl.ACTION.NONE; // disable wheel
+    instance.mouseButtons.wheel = 0; // disable wheel
+  }, []);
+  // useEffect(() => {
+  //   if (!controls) return;
+
+  //   controls?.setTarget(0, 3, 0);
+  //   controls.mouseButtons.wheel = CameraControlsImpl.ACTION.NONE; // disable wheel
+  // }, [controls]);
+
+  //
+  // curve
+  //
+
+  // const curve1Ref = useRef<Line2 | null>(null);
+
+  // const curve1CallbackRef = useCallback((instance: Line2) => {
+  //   curve1Ref.current = instance;
+  //   console.log("curve1Ref", instance);
+  // }, []);
 
   //
   // [space] play/pause
@@ -161,14 +209,6 @@ function Scene() {
   //   return () => unsubscribeJump();
   // }, [subscribeKeys]);
 
-  const cameraControlsCallbackRef = useCallback((instance: CameraControls) => {
-    cameraControlsRef.current = instance;
-    if (!instance) return;
-
-    instance.setTarget(0, 3, 0);
-    instance.mouseButtons.wheel = CameraControlsImpl.ACTION.NONE; // disable wheel
-  }, []);
-
   return (
     <Layout>
       <CameraControls ref={cameraControlsCallbackRef} />
@@ -178,6 +218,19 @@ function Scene() {
         rotation-x={-Math.PI / 12}
         screenTexture={videoTexture}
       />
+
+      {/* <CatmullRomLine
+        ref={curve1CallbackRef}
+        points={[
+          [-3, 2, 1],
+          [2, 2, 0],
+          [-1, 0, 3],
+          [2, -1, 0],
+        ]} // Array of Points
+        color="#ec36a0"
+        lineWidth={3} // In pixels (default)
+        segments={64}
+      /> */}
 
       <Sparkles
         count={20}
