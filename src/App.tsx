11eff41a296b9b1db121d306dd482c290d03851f
Antoine BERNIER <antoine.bernier@gmail.com>
2023-02-04 14:03:47 +0100

    dezoomFactor

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ function Scene() {
     showBoundingBox,
     fitting,
     centering,
+    dezoomFactor,
   } = useControls({
     cameraFrame: folder(
       {
@@ -90,6 +91,7 @@ function Scene() {
     showBoundingBox: true,
     fitting: true,
     centering: true,
+    dezoomFactor: { value: 0, min: 0, max: 5, step: 0.1 },
     iphone: folder(
       {
         rotX: { value: -Math.PI / 12, min: -Math.PI / 2, max: 0 },
@@ -353,6 +355,7 @@ function Scene() {
           h={_cameraFrameH}
           showBoundingSphere={showBoundingSphere}
           showBoundingBox={showBoundingBox}
+          dezoomFactor={dezoomFactor}
         />
       </Iphone>
 
@@ -379,6 +382,7 @@ type CameraFrameProps = {
   h?: number;
   showBoundingBox: boolean;
   showBoundingSphere: boolean;
+  dezoomFactor?: number;
 };
 
 // https://stackoverflow.com/a/73748435/133327
@@ -408,7 +412,17 @@ type CameraFrameAPI = {
 };
 
 const CameraFrame = forwardRef<CameraFrameAPI, CameraFrameProps>(
-  ({ y = 0, w = 7, h = 5, showBoundingBox, showBoundingSphere }, ref) => {
+  (
+    {
+      y = 0,
+      w = 7,
+      h = 5,
+      showBoundingBox,
+      showBoundingSphere,
+      dezoomFactor = 0,
+    },
+    ref
+  ) => {
     // const boxRef = useForwardRef(ref);
     const boxRef = useRef<THREE.Mesh>(null);
 
@@ -440,6 +454,7 @@ const CameraFrame = forwardRef<CameraFrameAPI, CameraFrameProps>(
       if (!boxRef?.current) return; // https://stackoverflow.com/a/62238917/133327
       bbox.setFromObject(boxRef.current);
       bbox.getBoundingSphere(bs);
+      bs.radius *= 1 + dezoomFactor;
 
       // Update boxHelper
       if (boxHelperRef.current) boxHelperRef.current.update();
